6841f9a9b9a56ec495730441908052e3bd521723
onoasu <64629201+onoasu@users.noreply.github.com>
2022-06-14 00:18:07 +0900

    Add 検索バーコンポーネント化

---
+++ components/home/homeSearch.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-row>
+    <v-col>
+      <v-text-field label="検索" type="text" v-model="search_keyword">
+        <template v-slot:append>
+          <v-btn class="btn btn-info mb-1" color="primary" @click="searchKeyword()"
+            >検索</v-btn
+          >
+        </template>
+      </v-text-field>
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  data() {
+    return {
+      search_keyword: "",
+    }
+  },
+  methods: {
+    searchKeyword() {
+      if (this.search_keyword !== "") {
+        const apiUrl = "/api/&q=";
+        console.log(apiUrl + this.search_keyword);
+        this.$store.dispatch(
+          "headlines/loadHeadlines",
+          apiUrl + this.search_keyword
+        );
+      }
+    },
+  },
+});
+</script>
