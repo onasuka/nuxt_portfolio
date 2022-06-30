7b512e93477e3ff732dac4c27ca42e618b07286a
onoasu <64629201+onoasu@users.noreply.github.com>
2022-06-30 21:48:59 +0900

    add 型の定義を追加

---
+++ constants/index.ts
@@ -0,0 +1,38 @@
+type LoginInfo = {
+  email: string;
+  password: string;
+};
+
+type ArticleInfo = {
+  source: string;
+  author: string;
+  content: string;
+  description: string;
+  publishedAt: string;
+  slug: string;
+  title: string;
+  url: string;
+  urlToImage: string;
+};
+
+type WordInfo = {
+  word: object;
+  meaning: string;
+  slug: string;
+  isEditing: boolean
+};
+
+type UseInfo = {
+  use: string;
+  name: string;
+  email: string;
+};
+
+type RemoveWordInfo = {
+  word: {
+    word: string;
+    meaning: string;
+    slug: string;
+    isEditing: boolean
+  };
+};
