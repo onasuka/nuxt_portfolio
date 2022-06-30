028f10a4fd243a0553ddbf34764768925126f12a
onoasu <64629201+onoasu@users.noreply.github.com>
2022-06-30 21:48:43 +0900

    change axios初期化を追加

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -1,5 +1,9 @@
 import { Plugin } from '@nuxt/types'
+import { initializeAxios } from '~/utils/api'
+
 const accessor: Plugin = ({ $axios }) => {
+  initializeAxios($axios)
+  
   $axios.onRequest((config) => {
     config.headers.common.Authorization = process.env.AXIOS_API_KEY
   })
