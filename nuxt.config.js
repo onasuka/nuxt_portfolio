e7c178ae9127eb7fa1b1783707b3dc7ab112ebac
onoasu <sssm0rt.a@gmail.com>
2022-07-25 00:22:44 +0900

    Update　タイトル修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: '20220302_app',
+    title: '英語アプリ',
     htmlAttrs: {
       lang: 'en'
     },
